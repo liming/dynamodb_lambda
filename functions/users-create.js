fd0f18432121da8b161c68c6d305bcb9a4ccb738
Ming Li <liming@gmail.com>
2021-04-18 22:59:43 +1000

    save take the plain password and encrypt it using KMS with a user-defined CMK

--- functions/users-create.js
+++ functions/users-create.js
@@ -3,90 +3,106 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const AWS = require('aws-sdk');
 const uuid = require('uuid');
+const { encryptPassword } = require('../lib/utils');
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.handler = (event, context, callback) => {
   const data = JSON.parse(event.body);
 
-  createUser(data, callback);
+  createUser(data)
+    .then((result) => callback(null, result))
+    .catch((err) => callback(err));
 };
 
-const createUser = (data, callback) => {
+const createUser = async (data) => {
   // validate input data
   // ideally this should be done in API gateway so lambdas does not have to be invoked
   if (!data || !data.email || !data.username || !data.credentials) {
-    return callback(null, {
+    return {
       statusCode: 400,
       headers: { 'Content-Type': 'text/plain' },
       body: 'Invalid request',
-    });
+    };
   }
 
-  const userItem = {
-    id: uuid.v1(),
-    username: data.username,
-    firstName: data.firstName,
-    lastName: data.firstName,
-    email: data.email,
-    credentials: data.credentials,
-  };
+  // encrypt password
+  const encryptedCredentials = await encryptPassword(data.credentials);
 
-  const transactItems = [
-    {
-      Put: {
-        TableName: process.env.USERS_TABLE,
-        // check not exists before (no user with this ID)
-        ConditionExpression: 'attribute_not_exists(#pk)',
-        ExpressionAttributeNames: {
-          '#pk': 'id',
-        },
-        // the attributes
-        Item: userItem,
-      },
-    },
-    {
-      Put: {
-        TableName: process.env.UNIQUES_TABLE,
-        // check not exists before (no user with this email)
-        ConditionExpression: 'attribute_not_exists(#pk)',
-        ExpressionAttributeNames: {
-          '#pk': 'value',
-        },
-        // the item attributes
-        Item: {
-          value: `UNIQUE#EMAIL:${data.email}`,
-        },
-      },
-    },
-    {
-      Put: {
-        TableName: process.env.UNIQUES_TABLE,
-        // check not exists before (no user with this email)
-        ConditionExpression: 'attribute_not_exists(#pk)',
-        ExpressionAttributeNames: {
-          '#pk': 'value',
-        },
-        // the item attributes
-        Item: {
-          value: `UNIQUE#USERNAME:${data.username}`,
-        },
-      },
-    },
-  ];
+  try {
+    const userItem = {
+      id: uuid.v1(),
+      username: data.username,
+      firstName: data.firstName,
+      lastName: data.firstName,
+      email: data.email,
+      credentials: encryptedCredentials,
+    };
 
-  return dynamoDb.transactWrite({ TransactItems: transactItems }, (err) => {
-    if (err) {
-      return callback(null, {
-        statusCode: err.statusCode || 501,
-        headers: { 'Content-Type': 'text/plain' },
-        body: `Could not create new user: ${err.message}`,
+    return await saveUser(userItem);
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 501,
+      headers: { 'Content-Type': 'text/plain' },
+      body: `Could not create new user: ${err.message}`,
+    };
+  }
+};
+
+const saveUser = (userItem) => {
+  return new Promise((resolve, reject) => {
+    const transactItems = [
+      {
+        Put: {
+          TableName: process.env.USERS_TABLE,
+          // check not exists before (no user with this ID)
+          ConditionExpression: 'attribute_not_exists(#pk)',
+          ExpressionAttributeNames: {
+            '#pk': 'id',
+          },
+          // the attributes
+          Item: userItem,
+        },
+      },
+      {
+        Put: {
+          TableName: process.env.UNIQUES_TABLE,
+          // check not exists before (no user with this email)
+          ConditionExpression: 'attribute_not_exists(#pk)',
+          ExpressionAttributeNames: {
+            '#pk': 'value',
+          },
+          // the item attributes
+          Item: {
+            value: `UNIQUE#EMAIL:${userItem.email}`,
+          },
+        },
+      },
+      {
+        Put: {
+          TableName: process.env.UNIQUES_TABLE,
+          // check not exists before (no user with this email)
+          ConditionExpression: 'attribute_not_exists(#pk)',
+          ExpressionAttributeNames: {
+            '#pk': 'value',
+          },
+          // the item attributes
+          Item: {
+            value: `UNIQUE#USERNAME:${userItem.username}`,
+          },
+        },
+      },
+    ];
+
+    return dynamoDb.transactWrite({ TransactItems: transactItems }, (err) => {
+      if (err) {
+        return reject(err);
+      }
+
+      return resolve({
+        statusCode: 200,
+        body: JSON.stringify(userItem),
       });
-    }
-
-    return callback(null, {
-      statusCode: 200,
-      body: JSON.stringify(userItem),
     });
   });
 };
